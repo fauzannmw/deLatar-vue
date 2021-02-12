1d111ca974a78609983b354a328c688544ca9c19
Muhamad Fauzan <fauzanwahyudi0@gmail.com>
2021-02-12 09:01:37 +0700

    first commit

---
+++ src/components/PlantStore.vue
@@ -0,0 +1,123 @@
+<template>
+  <!-- Plant Banner Section Begin -->
+  <section class="plant-banner spad">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-12 mt-5">
+          <carousel
+            class="product-slider"
+            :items="4"
+            :nav="false"
+            :autoplay="true"
+            :dots="false"
+          >
+            <div class="product-item">
+              <div class="pi-pic">
+                <img src="img/Plant1.jpg" alt="" />
+                <ul>
+                  <li class="w-icon active">
+                    <a href="#"><i class="icon_bag_alt"></i></a>
+                  </li>
+                  <li class="quick-view">
+                    <router-link to="/product">+ Quick View</router-link>
+                  </li>
+                </ul>
+              </div>
+              <div class="pi-text">
+                <div class="catagory-name">Plant</div>
+                <router-link to="/product"><a href="#">
+                  <h5>Eucalyptus</h5>
+                </a></router-link>
+                <div class="product-price">
+                  $14.00
+                  <span>$35.00</span>
+                </div>
+              </div>
+            </div>
+            <div class="product-item">
+              <div class="pi-pic">
+                <img src="img/Plant2.jpg" alt="" />
+                <ul>
+                  <li class="w-icon active">
+                    <a href="#"><i class="icon_bag_alt"></i></a>
+                  </li>
+                  <li class="quick-view"><a href="#">+ Quick View</a></li>
+                </ul>
+              </div>
+              <div class="pi-text">
+                <div class="catagory-name">Plant</div>
+                <a href="#">
+                  <h5>Bonsai</h5>
+                </a>
+                <div class="product-price">
+                  $13.00
+                </div>
+              </div>
+            </div>
+            <div class="product-item">
+              <div class="pi-pic">
+                <img src="img/Plant3.jpg" alt="" />
+                <ul>
+                  <li class="w-icon active">
+                    <a href="#"><i class="icon_bag_alt"></i></a>
+                  </li>
+                  <li class="quick-view"><a href="#">+ Quick View</a></li>
+                </ul>
+              </div>
+              <div class="pi-text">
+                <div class="catagory-name">Plant</div>
+                <a href="#">
+                  <h5>Decorative plants</h5>
+                </a>
+                <div class="product-price">
+                  $34.00
+                </div>
+              </div>
+            </div>
+            <div class="product-item">
+              <div class="pi-pic">
+                <img src="img/Plant4.jpg" alt="" />
+                <ul>
+                  <li class="w-icon active">
+                    <a href="#"><i class="icon_bag_alt"></i></a>
+                  </li>
+                  <li class="quick-view"><a href="#">+ Quick View</a></li>
+                  <li class="w-icon">
+                    <a href="#"><i class="fa fa-random"></i></a>
+                  </li>
+                </ul>
+              </div>
+              <div class="pi-text">
+                <div class="catagory-name">Plant</div>
+                <a href="#">
+                  <h5>Orchid</h5>
+                </a>
+                <div class="product-price">
+                  $34.00
+                </div>
+              </div>
+            </div>
+          </carousel>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Plant Banner Section End -->
+</template>
+
+<script>
+import carousel from "vue-owl-carousel";
+
+export default {
+  name: "PlantStore",
+  components: {
+    carousel,
+  },
+};
+</script>
+
+<style scoped>
+.product-item {
+  margin-right: 25px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderStore />
+    <HeroStore />
+    <PlantStore/>
+    <InstaStore/>
+    <PartnerStore/>
+    <FooterStore/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import HeaderStore from "@/components/HeaderStore.vue";
+import HeroStore from "@/components/HeroStore.vue";
+import PlantStore from "@/components/PlantStore.vue";
+import InstaStore from "@/components/InstaStore.vue";
+import PartnerStore from "@/components/PartnerStore.vue";
+import FooterStore from "@/components/FooterStore.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderStore,
+    HeroStore,
+    PlantStore,
+    InstaStore,
+    PartnerStore,
+    FooterStore,
+  },
+};
+</script>
